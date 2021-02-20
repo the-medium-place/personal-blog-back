5851345ab4c826591a81979fc822cf22fae51e1c
ZGS <zgstowell@gmail.com>
2021-02-19 23:56:21 -0800

    integrate portolio app /contactme route

--- controllers/index.js
+++ controllers/index.js
@@ -6,9 +6,51 @@ const postRoutes = require('./postController');
 const commentRoutes = require('./commentController');
 const tagRoutes = require('./tagController');
 
+const nodemailer = require('nodemailer');
+
 router.get('/', (req,res) => {
     res.send('Welcome to zac\'s blog api-side!');
 })
+
+router.get('/contactme', (req,res) => {
+    res.send('this is a new jam...')
+})
+
+
+router.post('/contactme', function ({ body }, res) {
+    // Instantiate the SMTP server
+    const smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    })
+
+    // Specify what the email will look like
+    const mailOpts = {
+        from: 'Your sender info here', // This is ignored by Gmail
+        to: 'zgstowell@gmail.com',
+        subject: 'New message from portfolio page!',
+        text: `Message from: ${body.username}\n\n Email: ${body.userEmail}\n\n Message:\n ${body.userMessage}`
+    }
+
+    // Attempt to send the email
+    smtpTrans.sendMail(mailOpts, (error, response) => {
+        if (error) {
+            console.log(error);
+            res.send(error) // Show a page indicating failure
+        }
+        else {
+            console.log(response);
+            res.status(200).send('success') // Show a page indicating success
+        }
+    })
+
+})
+
 
 
 router.use('/api/comments', commentRoutes)
